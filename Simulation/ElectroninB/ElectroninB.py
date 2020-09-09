4a24bc3e401e458b7fcbc9caea00168cfe17db82
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2020-09-09 17:53:08 +0530

    Create ElectroninB.py

---
+++ Simulation/ElectroninB/ElectroninB.py
@@ -0,0 +1,29 @@
+from vpython import *
+import numpy as np
+
+display(width = 1300, height = 1000)
+
+MagneticField = 1e-5*vector(5,0,0)
+
+Electron = sphere(pos = vector(-5,0,0), radius = 0.1, color =color.white, make_trail = True)
+
+Electron.m = 9.1e-31
+Electron.charge = -1.6e-19
+Electron.v = 7.5e6
+Electron.p = Electron.m*Electron.v*vector(0,3,0)
+
+
+t = 0
+dt = 13.87e-9
+
+while t<1e-6:
+    rate(1000)
+      
+    Force = cross(Electron.charge* Electron.p/ Electron.m, MagneticField)
+    # update momentum
+    Electron.p = Electron.p+ Force*dt
+    #update position
+    Electron.pos = Electron.pos + Electron.p*dt/Electron.m
+
+    t = t+dt
+                                                                                                                                              
