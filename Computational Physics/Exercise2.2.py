fd9cc4b73559e5a7b5e0026a2d61f9c99ec1ff18
Sudhanva Lalit <sudhanvalalit@github.com>
2020-09-27 13:49:03 -0400

    edited plot

--- Computational Physics/Exercise2.2.py
+++ Computational Physics/Exercise2.2.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
+sns.set()
 
 # Constants
 G = 6.67e-11
@@ -13,8 +15,16 @@ def main():
     h = np.empty(1000)
     for i in range(1000):
         h[i] = (G*M*T[i]**2/(4.0*np.pi**4))**(1.0/3) - R
-    plt.plot(T, h, 'r-')
+    sns.set_style("dark")
+    fig = plt.figure(figsize=(7, 7))
+    ax1 = plt.axes()
+    ax1.set_xlabel('Time (s)')
+    ax1.set_ylabel('Height (m)')
+    ax1.set_title('Height vs Time')
+    ax1.plot(T, h)
     plt.show()
+    ax1.legend()
+    figure.savefig('SatelliteHeight.pdf')
 
 
 if __name__ == "__main__":
