4321532ad87a5e0e9b1172f90d2013ab83eb2266
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2020-09-29 20:11:01 +0530

    Create Exerise2.4(b).py

---
+++ Computational Physics/Exerise2.4(b).py
@@ -0,0 +1,8 @@
+import numpy as np
+
+d = int(input("Enter Distance of planet :"))
+v = float(input("Enter Velocty of planet :"))
+
+t = d/v
+
+print("The time taken by spaceship is :", t, "light years")
