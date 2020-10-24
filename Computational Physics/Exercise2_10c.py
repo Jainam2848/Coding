d13efd1b63576e7104661ef0c806b879da279ead
Sudhanva Lalit <sudsid@gmail.com>
2020-10-24 14:47:30 -0400

    updated 10d

--- Computational Physics/Exercise2_10c.py
+++ Computational Physics/Exercise2_10c.py
@@ -1,9 +1,10 @@
 import numpy as np
-import math
-from array import *
-a1 = 15.67
-a2 = 17.23
-a3 = 0.75
+
+# The values are picked from wikipedia on the semi empirical mass formula
+# Wapstra fit to the data
+a1 = 15.67  # 15.8
+a2 = 17.23  # 18.3
+a3 = 0.75  # 0.714
 a4 = 93.2
 
 
@@ -20,8 +21,8 @@ def main():
             else:
                 a5 = -12.0
 
-        B[A] = float(a1*A - a2*A**(2/3) - a3*(Z**2)/A **
-                     (1/3) - a4*((A-2*Z)**2)/A + a5/A**(1/2))
+        B[A] = a1*A - a2*A**(2/3) - a3*(Z**2)/(A ** (1/3)) - \
+            a4*((A-2*Z)**2)/A + a5/A**(1/2)
 
     A = np.where(B == np.max(B))[0][0]
 
--- Computational Physics/Exercise2_10d.py
+++ Computational Physics/Exercise2_10d.py
@@ -4,11 +4,10 @@ a2 = 17.23
 a3 = 0.75
 a4 = 93.2
 
-# now we have many value for a5
-B = np.zeros(300)
-
 
 def main():
+    B = np.zeros(300)
+    A1 = np.zeros(100)
     for Z in range(1, 100):
         for A in range(Z, 3*Z):
             if A % 2 != 0:
@@ -23,14 +22,15 @@ def main():
             a4*((A-2*Z)**2)/A + a5/A**(1/2)
 
         A = np.where(B == np.max(B))[0][0]
-        Z = np.where(B == np.max(B))[0][0]
+        A1[Z] = A
+    Z = np.where(A1 == np.max(A1))[0][0]
 
 # question c
-        print(f"The binding energy of atom is : {B[A]/A}")
+    print(f"The binding energy of atom is : {B[A]/A}")
 
-        print(f"The value of A for most stable nucleus is : {A}")
+    print(f"The value of A for most stable nucleus is : {A}")
 
-        print(f"The value of A for most stable nucleus is : {A}")
+    print(f"The value of Z for most stable nucleus is : {Z}")
 
 
 if __name__ == "__main__":
