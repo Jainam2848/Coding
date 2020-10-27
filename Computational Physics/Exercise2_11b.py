1f2fb4b54966471916edae2900436a9e3eeb9fdb
Sudhanva Lalit <sudsid@gmail.com>
2020-10-27 10:18:05 -0400

    solved prblm 2_11b

--- Computational Physics/Exercise2_11b.py
+++ Computational Physics/Exercise2_11b.py
@@ -1,11 +1,16 @@
 import math
 
-N = int(input("Enter a number :"))
-def Binomial(n,k):
-    Binomial = math.comb(n,k)
-    for n in range(N):
-        for k in range(n):
-            print(Binomial(n,k), " ")
-            print("\n")
-    
-    Binomial(n,k) 
+
+def Binomial(N):
+    for n in range(1, N+1):
+        [print(math.comb(n, k), end=" ") for k in range(n+1)]
+        print(end="\n")
+
+
+def main():
+    N = int(input("Enter a number :"))
+    Binomial(N)
+
+
+if __name__ == "__main__":
+    main()
