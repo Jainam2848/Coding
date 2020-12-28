8ad3df0526f3ed3beb72e4ed68dcb3df7643885c
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2020-12-28 11:42:54 +0530

    Create Exercise2_13(b).py

---
+++ Computational Physics/Chapter2/Exercise2_13(b).py
@@ -0,0 +1,9 @@
+def gcd(a,b):
+    if(b==0):
+        return a
+    else:
+        return gcd(b,a%b)
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("GCD is: ")
+print(gcd(a, b))
