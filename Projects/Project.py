afab05915f0795925a8403407856eb6bcbe4e0a8
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-01-03 22:23:24 +0530

    Create Project.py

---
+++ Projects/Project.py
@@ -0,0 +1,41 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from numpy import loadtxt
+from array import array
+
+#Graph of Average temperature vs years.
+
+data = np.loadtxt("graph.txt")
+x = data[:,0]   #Years
+y = data[:,1]   #Average temperature
+
+plt.xlabel("Years")
+plt.ylabel("Average Temperature")
+plt.title("Graph of Average Temperature since 1880")
+plt.grid(True)
+plt.plot(x,y)
+
+plt.show()
+
+#Graph of sunspots vs years
+
+Data = np.loadtxt('sunspot.txt')
+a = Data[:,0]
+b = Data[:,1]
+
+plt.xlabel("Number of years")
+plt.ylabel("Number of sunspots")
+plt.title("Graph of Sunspots")
+plt.grid(True)
+plt.plot(a,b)
+plt.show()
+
+#Graph of sunspots vs temperature
+
+plt.xlabel("Temperature")
+plt.ylabel("Sunspots")
+plt.title("Effect of sunspots on Global average Temperature")
+plt.grid(True)
+plt.plot(y,b)
+plt.show()
+
