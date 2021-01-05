6a0aba61052ee9160dd7600aaadf200a0ee034c8
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-01-05 11:06:30 +0530

    Update Project.py

--- Projects/Project.py
+++ Projects/Project.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from numpy import loadtxt
 from array import array
+from mpl_toolkits.mplot3d import Axes3D
 
 # Graph of Average temperature vs years.
 
@@ -20,14 +21,15 @@ plt.show()
 # Graph of sunspots vs years
 
 Data = np.loadtxt('sunspot.txt')
-a = Data[:, 0]
-b = Data[:, 1]
+a = Data[:, 0]     #years
+b = Data[:, 1]     #sunspots
 
 plt.xlabel("Number of years")
 plt.ylabel("Number of sunspots")
 plt.title("Graph of Sunspots")
 plt.grid(True)
 plt.plot(a, b)
+
 plt.show()
 
 # Graph of sunspots vs temperature
@@ -36,7 +38,51 @@ plt.xlabel("Temperature")
 plt.ylabel("Sunspots")
 plt.title("Effect of sunspots on Global average Temperature")
 plt.grid(True)
-plt.plot(y, b[:140], 'k.')
+plt.plot(y, b, 'k.')
+
 plt.show()
 
 # 3D plot of temperature and sunspots as a function of years.
+
+fig = plt.figure()
+ax = fig.add_subplot(111,projection="3d")
+ax.set_xlabel("Years")
+ax.set_ylabel("Average Temperature")
+ax.set_zlabel("Sunspots")
+ax.set_title("3D plot of temperature and sunspots as a function of years")
+
+ax.scatter(x,y,b, color='red')
+
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
