7e86552fa3e6ede983c7aa5a920dc9f020edc584
Sudhanva Lalit <sudsid@gmail.com>
2021-02-06 21:25:56 -0500

    added 3.4b

--- Computational Physics/Chapter3/Exercise3.4.py
+++ Computational Physics/Chapter3/Exercise3.4.py
@@ -1,17 +1,17 @@
 import vpython as v
+
+# 3.4 (a)
 L = 5
 R = 0.3
 count = 1
-for i in  range(-L,L+1):
-    for j in  range(-L,L+1):
-        for k in range(-L,L+1):
-            if count%2==0:
-                v.sphere(pos = v.vector(i,j,k), radius = R,  color = v.color.red)
+for i in range(-L, L+1):
+    for j in range(-L, L+1):
+        for k in range(-L, L+1):
+            if count % 2 == 0:
+                v.sphere(pos=v.vector(i, j, k), radius=R,  color=v.color.red)
             else:
-                #chlorine
-                v.sphere(pos = v.vector(i,j,k), radius = R, color = v.color.blue)
-            count+=1   
-    
-        print(i,j,k)
-    
-    
+                # chlorine
+                v.sphere(pos=v.vector(i, j, k), radius=R, color=v.color.blue)
+            count += 1
+
+        print(i, j, k)
