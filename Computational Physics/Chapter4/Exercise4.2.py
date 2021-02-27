e159ca809313e9027df6506e15158b7f47ed30d0
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-02-27 20:34:33 +0530

    Update Exercise4.2.py

--- Computational Physics/Chapter4/Exercise4.2.py
+++ Computational Physics/Chapter4/Exercise4.2.py
@@ -7,17 +7,18 @@ c = float(input('Enter value of c :'))
 def main():
 #4.1(a)
     x = (-b + sqrt(b ** 2 - 4 * a * c)) / (2 * a)
-    y = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a)
+    y = (-b - sqrt(b ** 2 - 4 * a * c)) / (2 * a) #correct value
     
     print("The first solution using standard formula is :", x)
     print("The second solution using standard formula is :", y)
 
 #4.2(b)    
-    x1 =  2*c/(-b - sqrt(b**2 - 4*a*c))
+    x1 =  2*c/(-b - sqrt(b**2 - 4*a*c))  #correct value
     y2 =  2*c/(-b + sqrt(b**2 - 4*a*c))
     
-    print("The first solution with alternative formula is :", x1)
-    print("The second solution with alternative formula is :", y2)
+    print("")
+    print("The first solution with method B is :", x1)
+    print("The second solution with method B  is :", y2)
     
 if __name__ == "__main__":
     main()
