d8865d20abe772c71b813d4610ade617df46568c
Sudhanva Lalit <sudsid@gmail.com>
2021-03-02 13:14:53 -0500

    edited 5.1

--- Computational Physics/Chapter5/Exercise5.1.py
+++ Computational Physics/Chapter5/Exercise5.1.py
@@ -1,24 +1,29 @@
 import numpy as np
 import matplotlib.pylab as plt
 
-data = np.loadtxt('http://www-personal.umich.edu/~mejn/cp/data/velocities.txt', float)
+data = np.loadtxt(
+    'http://www-personal.umich.edu/~mejn/cp/data/velocities.txt', float)
 
 t = data[:, 0]
 v = data[:, 1]
 #x = sum(v*t)
 
-def f(v,t):
-    return v*t
 
-#print(x)    
-n = len(v)   #number of slices
-b = 100      #upper limit
-a = 0        #lower limit
-h = (b-a)/n
+def f(v, t):
+    return v * t
 
-s = []
-for k in range(1,n):
-    s+= f(v[a+k*h], t[a+k*h])
 
-print(h*s)
-plt.plot(t,v)
+# print(x)
+n = len(v)  # number of slices
+b = 100  # upper limit
+a = 0  # lower limit
+h = (b - a) / n
+
+s = 0.0
+for k in range(n):
+    print(v[k], t[k])
+    s += f(v[k], t[k])
+
+print(s)
+plt.plot(t, v)
+plt.show()
