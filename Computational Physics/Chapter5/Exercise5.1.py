8d5e6a54fa35e4f109bc2ced431b0df0df536e2f
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-03-02 20:57:17 +0530

    Update Exercise5.1.py

--- Computational Physics/Chapter5/Exercise5.1.py
+++ Computational Physics/Chapter5/Exercise5.1.py
@@ -5,26 +5,20 @@ data = np.loadtxt('http://www-personal.umich.edu/~mejn/cp/data/velocities.txt', 
 
 t = data[:, 0]
 v = data[:, 1]
-x = sum(v*t)
+#x = sum(v*t)
 
-def f(x):
-    f = v*t
-    return f
+
 
 #print(x)    
-n = 100   #number of slices
-b = 100    #upper limit
-a = 0      #lower limit
+n = len(v)   #number of slices
+b = 100      #upper limit
+a = 0        #lower limit
 h = (b-a)/n
 
-s = 0.5*f(a) + 0.5*f(b)
+s = []
 
 for k in range(1,n):
-    s+= f(a+h*k)
+    s+= v(a+h*k)*t(a+h*k)
 
 print(h*s)
 plt.plot(t,v)
-#plt.plot(t,s)
-plt.xlabel("Time")
-
-plt.show()
