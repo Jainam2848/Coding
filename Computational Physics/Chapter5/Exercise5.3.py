d2e5e9ef1ed43b821511405598a6a10c8804a617
sudhanvalalit <sudsid@gmail.com>
2021-03-19 10:14:43 -0400

    Fixed exercise 5.3
    Fixed the code. Plot works fine now.

--- Computational Physics/Chapter5/Exercise5.3.py
+++ Computational Physics/Chapter5/Exercise5.3.py
@@ -1,28 +1,27 @@
 import numpy as np
-import math 
+import math
 import matplotlib.pylab as plt
 
 
 def f(t):
     return math.exp(-(t**2))
 
+
 a = 0
-N = 100
+N = 1000
 h = 0.0
 
-
-for b in np.arange(0,3,0.1):
+x = np.arange(0.0, 3.0, 0.1)
+z = np.zeros(len(x))
+for i in range(len(x)):
+    b = x[i]
     h += (b-a)/N
-    s = 0.5 *(f(a)+f(b))
-    #print(h)
-    
-    for k in range(1,N):
-        s+= f(a + k*h)
+    s = 0.5 * (f(a)+f(b))
+    # print(h)
+
+    for k in range(1, N):
+        s += f(a + k*h)
     c = h*s
-    z = np.array([c])
-    #print(z)
-    y = np.arange(0, 3, 0.1)
-    b = np.array([y])
-    #print(b)
-    plt.plot(z,b, linestyle='-.')
-    plt.show()
+    z[i] = c
+plt.plot(x, z, linestyle='-.')
+plt.show()
