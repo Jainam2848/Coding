1bfd05386e3a8fa386be82e9446588e6d65793aa
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-07-14 11:53:14 +0530

    Create Project2.py

---
+++ Projects/Project2.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import array as arr
+
+
+data = np.loadtxt("graph.txt")
+
+#d1,d2,d3 = data
+print(type(data),np.shape(data))
+#print(type(d1))
+
+#plt.plot(d1,d2)
+
+#plt.show()
