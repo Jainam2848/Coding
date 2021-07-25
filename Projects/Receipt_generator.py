53e47f90a565ad3bdffd0fe69674899848dfe1b0
Jainam2848 <69759971+Jainam2848@users.noreply.github.com>
2021-07-25 12:04:24 +0530

    Create Receipt_generator.py

---
+++ Projects/Receipt_generator.py
@@ -0,0 +1,123 @@
+import numpy as np
+import speech_recognition as sr
+import pyttsx3 as p
+
+engine = p.init("sapi5")
+
+voices = engine.getProperty("voices")
+engine.connect("voices", voices[0].id)
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+    
+def listen():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        query = ""
+        try:
+            print("Recognizing....")
+            query = r.recognize_google(audio)
+            print(f"User said : {query}")
+        except Exception as e:
+            speak("Say that again!")
+            return "none"
+    return query
+#listen()
+
+        
+speak("Welcome to Ashirwad kirana store")
+speak('We have following items available at the moment :')
+
+print('We have following items available at the moment :')
+
+items = ['Garam Masala', 'Almonds', 'Kaju', 'Chilli powder', 'Turmeric powder']
+
+prices = [300, 800, 900, 200, 45]
+customer_list = []
+total_bill = 0
+i = 0
+'''
+while  i<len(items):
+    print('1 kg price of',items[i],'is :', prices[i])
+    i+=1 
+ 
+for i in range(len(items)):
+    items[i] = items[i].lower()
+    
+while True: 
+    
+    speak("What would you like to purchase ? or say exit :")
+    #user_input = input('What would you like to purchase ? or press q to exit  :').lower()
+    query = listen().lower()
+    
+    print("listening..")
+    if 'exit' in query:
+        break
+    for a in items:
+        if a[0] not in query:
+            print("Please choose your item from the list.")
+        else:
+            pass
+        
+    if 'exit' in query:
+        break
+    
+    elif 'garam masala' in query or 'garam' in query :
+        speak("garam masala has been added ")
+        customer_list.append(query)
+        speak("How much kilogram :")
+        query1 = listen() 
+        
+        total_bill =  total_bill + int(query1)*300
+        
+    elif 'almonds' in query or 'alomond' in query:
+        speak("almonds have been added ")
+        customer_list.append(query)
+        speak("How much kilogram :")
+        query1 = listen() 
+        
+        total_bill =  total_bill + int(query1)*800
+        
+    elif 'kaju' in query:
+        speak("kaju has been added ")
+        customer_list.append(query)
+        speak("How much kilogram :")
+        query1 = listen() 
+        
+        total_bill =  total_bill + int(query1)*900
+        
+    elif 'chilli powder' in query:
+        speak("chilli powder has been added ")
+        customer_list.append(query)
+        speak("How much kilogram :")
+        query1 = listen() 
+        
+        total_bill =  total_bill + int(query1)*200
+        
+    elif 'turmeric powder'in query or 'turmeric' in query:
+        speak("turmeric powder has been added ")
+        customer_list.append(query)
+        speak("How much kilogram :")
+        query1 = listen() 
+        
+        total_bill =  total_bill + int(query1)*30
+'''
+print()    
+print("Your list of items is :",[customer_list])   
+
+speak("Your list of items is :")
+
+speak(customer_list) 
+print("Your total bill is ")
+  
+speak("Your total bill is ")
+
+print(total_bill)
+
+
+speak("Thank you for Shopping with us !🙏")
